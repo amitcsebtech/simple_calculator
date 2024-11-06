a98c546563208ca33c625974996ec6c683c09fec
amitcsebtech <153449422+amitcsebtech@users.noreply.github.com>
2024-11-07 01:00:52 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+function appendChar(char) {
+    document.getElementById("display").value+=char;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value='';
+}
+
+
+function deleteChar() {
+    let display=document.getElementById("display").value;
+    document.getElementById("display").value=display.slice(0,-1);
+}
+
+
+function calculateResult() {
+    let expression=document.getElementById("display").value;
+    
+   try {
+    document.getElementById("display").value=eval(expression);
+   }catch (error) {
+    document.getElementById("display").value="Error";
+   }
+   
+}
+
